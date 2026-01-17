cd95d413e3d2d5f9033a1d42088a14fd938ff130
EmanSan9 <sanna950@gmail.com>
2026-01-17 21:25:48 +0100

    Meta description added, font linked in the head

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,31 @@ const Footer = () => {
     <footer className="py-6 text-center text-md text-white">
       <div className="flex gap-4 justify-center py-3">
         <span>
-          <a href="https://www.linkedin.com/in/emanuelesanna/">
-            <GrLinkedin className="text-white" size={"1.5rem"} />
+          <a
+            href="https://www.linkedin.com/in/emanuelesanna/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Profilo Linkedin di Emanuele Sanna"
+          >
+            <GrLinkedin
+              aria-hidden="true"
+              className="text-white"
+              size={"1.5rem"}
+            />
           </a>
         </span>
         <span>
-          <a href="https://github.com/ESan9">
-            <GrGithub className="text-white" size={"1.5rem"} />
+          <a
+            href="https://github.com/ESan9"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Profilo GitHub di Emanuele Sanna"
+          >
+            <GrGithub
+              aria-hidden="true"
+              className="text-white"
+              size={"1.5rem"}
+            />
           </a>
         </span>
       </div>
